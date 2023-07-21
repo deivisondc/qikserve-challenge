72aacba9565afd40fe4a9f6038247e378bcb1608
Deivison Cardoso <deivisondc@gmail.com>
2023-07-21 11:21:55 -0300

    config editorconfig, eslint, prettier and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        copy: {
+          main: "#121212",
+          secondary: "#464646",
+          body: "#2c2c2c",
+          inactive: "#5f5f5f",
+        },
+        background: {
+          default: "#f8f9fa",
+          subtle: "#eeeeee",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
